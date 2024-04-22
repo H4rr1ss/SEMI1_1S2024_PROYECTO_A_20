29284e806b2d9aa2d90acdb9803ddfd37110b500
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2024-04-21 22:13:47 -0600

    [feat]: revision vistas

--- frontend/src/app/(airline)/tus-tickets/page.tsx
+++ frontend/src/app/(airline)/tus-tickets/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { useClientStore } from '@/store/store'
 import Ticket from '@/components/ui/ticket/Ticket';
+import { useClientStore } from '@/store/store'
 import './tickets.css'
 
 export default function Tickets() {
@@ -10,23 +10,22 @@ export default function Tickets() {
     <div className="tickets flex flex-col items-center flex-grow h-auto w-full mt-14">
       <h1>Mis viajes</h1>
       <div className="box flex flex-col items-center w-5/6 mb-8 h-auto">
-
-      {clientStore.tickets.map((t, index) => (
-        <Ticket
-          key={index}
-          origin={t.origin}
-          destination={t.destination}
-          idFlight={t.idFlight}
-          date={t.date}
-          typeFlight={t.typeFlight}
-          boardingTime={t.boardingTime}
-          arrivalTime={t.arrivalTime}
-          id={t.id}
-          lastName={clientStore.lastName}
-          flightPrice={t.flightPrice}
-          check={t.check}
-        />
-      ))}
+        {clientStore.tickets.map((t, index) => (
+          <Ticket
+            key={index}
+            origin={t.origin}
+            destination={t.destination}
+            idFlight={t.idFlight}
+            date={t.date}
+            typeFlight={t.typeFlight}
+            boardingTime={t.boardingTime}
+            arrivalTime={t.arrivalTime}
+            id={t.id}
+            lastName={clientStore.lastName}
+            flightPrice={t.flightPrice}
+            check={t.check}
+          />
+        ))}
       </div>
     </div>
   )
