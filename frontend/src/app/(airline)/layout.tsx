e8c913875fafdba6f1fce103b052d293f5e3d8e3
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2024-04-24 07:39:04 -0600

    [add]: Chatbot

--- frontend/src/app/(airline)/layout.tsx
+++ frontend/src/app/(airline)/layout.tsx
@@ -1,9 +1,19 @@
-import Footer from "@/components/ui/footer/Footer";
+'use client'
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 import TopMenu from "@/components/ui/top-menu/TopMenu";
+import Footer from "@/components/ui/footer/Footer";
+import { useEffect } from 'react';
 
 export default function AirlineLayout( { children }: {
   children: React.ReactNode;
 } ) {
+  useEffect(() => {
+    Kommunicate.init("3809f2f1a3caa627dcf92a8a49a7eaec8", {
+      automaticChatOpenOnNavigation: true,
+      popupWidget: true
+    });
+  }, []);
+
   return (
     <main className="flex flex-col h-screen">
         <TopMenu />
