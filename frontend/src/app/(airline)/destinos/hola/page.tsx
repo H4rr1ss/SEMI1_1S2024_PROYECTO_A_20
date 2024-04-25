590f309133423b27c8a8ea99b62488143c9a988e
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2024-04-25 00:52:11 -0600

    x

---
+++ frontend/src/app/(airline)/destinos/hola/page.tsx
@@ -0,0 +1,11 @@
+import DetalleDestino2 from "./nuevo";
+
+const DetalleDestino = () => {
+  return (
+      <div>
+          <DetalleDestino2 />
+      </div>
+  );
+}
+
+export default DetalleDestino;
